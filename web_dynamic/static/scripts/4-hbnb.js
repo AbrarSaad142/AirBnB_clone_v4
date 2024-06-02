23077bc604b39856b032b61e3330b70542c4eba8
Areejslama <Areegahmed449@gmail.com>
2024-06-01 19:38:57 -0700

    filter

--- web_dynamic/static/scripts/4-hbnb.js
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -26,38 +26,42 @@ $.get('http://0.0.0.0:5001/api/v1/status/', function (data, class_status) {
     }
 });
 $('.filters button').click(function() {
-	$.ajax({
-		type: 'post',
-		url: 'http://0.0.0.0:5001/api/v1/places_search',
-		dataType: 'json',
-		data: '{}',
-		contentType: 'application/json; charset=utf-8',
-	success: function (places) {
-	for (let i = 0; i < places.length; i++) {
-	$('.places').append(`<article>
-            <div class="title_box">
-            <h2> ${places[i].name}>/h2>
-            <div class="price_by_night"> ${places[i].price_by_night} </div>
-            </div>
-            <div class="information">
-            <div class="max_guest">${places[i].max_guest}
-            ${places[i].max_guest > 1 ? 'Guests' : 'Guest'} </div>
-            <div class="number_rooms">${places[i].number_rooms}
-            ${places[i].number_rooms > 1 ? 'Bedrooms' : 'Bedroom'}  </div>
-            <div class="number_bathrooms">${places[i].number_bathrooms}
-            ${places[i].number_bathrooms > 1 ? 'Bathrooms' : 'Bathroom'}  </div>
-            </div>
-            <div class="user">
-            </div>
-            <div class="description">
-            ${places[i].description}
-            </div>
-            </article>
-            `);
-        }
-    },
-    error: function (xhr, status) {
-        console.log('error ' + status);
-    }
-});
+        const List = Object.keys(selectedAmenities);
+        $.ajax({
+            type: 'POST',
+            url: 'http://0.0.0.0:5001/api/v1/places_search',
+            dataType: 'json',
+            data: JSON.stringify({ amenities: List }),
+            contentType: 'application/json; charset=utf-8',
+            success: function (places) {
+                for (let i = 0; i < places.length; i++) {
+                    $('.places').append(`<article>
+                        <div class="title_box">
+                            <h2> ${places[i].name} </h2>
+                            <div class="price_by_night"> ${places[i].price_by_night} </div>
+                        </div>
+                        <div class="information">
+                            <div class="max_guest">${places[i].max_guest}
+                                ${places[i].max_guest > 1 ? 'Guests' : 'Guest'}
+                            </div>
+                            <div class="number_rooms">${places[i].number_rooms}
+                                ${places[i].number_rooms > 1 ? 'Bedrooms' : 'Bedroom'}
+                            </div>
+                            <div class="number_bathrooms">${places[i].number_bathrooms}
+                                ${places[i].number_bathrooms > 1 ? 'Bathrooms' : 'Bathroom'}
+                            </div>
+                        </div>
+                        <div class="user">
+                        </div>
+                        <div class="description">
+                            ${places[i].description}
+                        </div>
+                    </article>`);
+                }
+            },
+            error: function (xhr, status) {
+                console.log('error ' + status);
+            }
+        });
+    });
 });
